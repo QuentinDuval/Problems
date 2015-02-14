2d1c85ab5011b5bf87f1c33ed6466d45faf6c41d
QuentinDuval <senmenty@gmail.com>
2015-02-14 16:53:26 +0100

    KitayutaMart
    Improvement but consumes too much memory

--- src/NumericProblems.cpp
+++ src/NumericProblems.cpp
@@ -11,24 +11,42 @@ namespace prob
     * Lun the dog will always by the cheapest apple.
     * We need to keep track of what price was the last apple he bought.
     */
+   static const int MOD = 1000000007;
+   using ModPrice = std::pair<int, int>;
+
+   static ModPrice makeModPrice(int i)
+   {
+      return std::make_pair(i / MOD, i % MOD);
+   }
+
+   static void doubleModPrice(ModPrice& p)
+   {
+      p.second *= 2;
+      if (p.second < MOD)
+         return;
+
+      p.first++;
+      p.second = p.second % MOD;
+   }
+
    int KitayutaMart::lastPrice(int n, int k)
    {
-      std::vector<long long> prices(k, 0);
+      std::vector<ModPrice> prices(k);
       for (int i = 0; i < k; ++i)
-         prices[i] = i + 1;
+         prices[i] = makeModPrice(i + 1);
 
       long long mostExpensive = 0;
-      auto comp = std::greater<int>();
+      auto comp = std::greater<ModPrice>();
       std::make_heap(begin(prices), end(prices), comp);
 
       for (int i = 0; i < n; ++i)
       {
          std::pop_heap(begin(prices), end(prices), comp);
-         mostExpensive = prices.back();
-         prices.back() *= 2;
+         mostExpensive = prices.back().second;
+         doubleModPrice(prices.back());
          std::push_heap(begin(prices), end(prices), comp);
       }
 
-      return mostExpensive % 1000000007;
+      return mostExpensive;
    }
 }
--- tests/NumericProblemsTests.cpp
+++ tests/NumericProblemsTests.cpp
@@ -20,7 +20,7 @@ namespace prob
       assert(2 == KitayutaMart::lastPrice(3, 2));
       assert(4 == KitayutaMart::lastPrice(5, 3));
 
-      //assert(570312504 == KitayutaMart::lastPrice(1000000000, 1));
+      assert(570312504 == KitayutaMart::lastPrice(1000000000, 1));
       //assert(493827168 == KitayutaMart::lastPrice(987654321, 876543210));
    }
 }
