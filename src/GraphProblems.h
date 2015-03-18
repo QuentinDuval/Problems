9fe992f916d0788d2ca5b898dc183c03cc8745ce
QuentinDuval <senmenty@gmail.com>
2015-03-18 21:40:06 +0100

    Revert

--- src/GraphProblems.h
+++ src/GraphProblems.h
@@ -69,28 +69,10 @@ namespace prob
    * http://community.topcoder.com/stat?c=problem_statement&pm=13596
    * ---------------------------------------------------------------
    *
-   * Alice will always to to select the shortest path and Bob will selectively use token to disturb this path
-   * 1. We start by computing the shortest path from every point to the destination (node N-1).
-   *    To do this a single Dijkstra is enough (if you create the reverse graph)
-   * 2. Then we proceed by dynamic programming, computing the shortest cost from all vertices to N-1, with an increasing number of token K:
-   *    - At iteration K = 0, the cost of the path from each vertices to N-1 is simply the shortest path
-   *    - At iteration K + 1, for every vertex V, we look at its neighnors, and compute the cost as
-   *      cost(V, K+1) = max [U having an edge to V] { cost(edge(U, V)) + cost(U, K) + cost(V, K) }
+   * Alice will not always select the shortest path...
    */
    struct MaliciousPath
    {
       static long minPath(int N, int K, std::vector<int> const& from, std::vector<int> const& to, std::vector<int> const& cost);
-
-      struct WeightedEdge
-      {
-         WeightedEdge(int to, int cost) : m_to(to), m_cost(cost) {}
-         int m_to;
-         int m_cost;
-      };
-
-      using Graph = std::vector<std::vector<WeightedEdge>>;
-
-   private:
-      static void shortestPathsFrom(Graph const& graph, int source, std::vector<long>& shortestLen, std::vector<long>& nbLinks, std::vector<int>& prevOnPath);
    };
 }
